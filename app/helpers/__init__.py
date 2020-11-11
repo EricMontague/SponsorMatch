df1840fa243066ebe833f900d33a1f557ea1172c
Eric Montague <eric.g.montague@gmail.com>
2020-11-11 00:12:48 -0500

    Imported new helper function

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -5,5 +5,12 @@ to be used in the application.
 
 from app.helpers.decorators import permission_required, admin_required
 from app.helpers.email import send_email
-from app.helpers.form_constants import TIME_FORMAT, STATES, PACKAGE_TYPES, PEOPLE_RANGES, TIMES
+from app.helpers.form_constants import (
+    TIME_FORMAT,
+    STATES,
+    PACKAGE_TYPES,
+    PEOPLE_RANGES,
+    TIMES,
+)
 from app.helpers.mixins import FormMixin, SearchableMixin
+from app.helpers.search import paginate_search
