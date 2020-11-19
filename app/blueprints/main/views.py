f1b02da0aae7017d6ff8eb33ae54752ec2c50b68
Eric Montague <eric.g.montague@gmail.com>
2020-11-18 20:19:49 -0500

    Abstracted query creation out of the view functions and finished the advanced_search route

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -8,7 +8,7 @@ from app.blueprints.main import main, services
 from app.blueprints.main.forms import AdvancedSearchForm, SearchForm
 from app.extensions import db
 from app.models import Event, Venue
-from app.common import paginate_search
+from app.common import paginate_search, QueryType
 
 
 @main.route("/")
@@ -19,80 +19,82 @@ def index():
     pagination = Event.query.filter(Event.is_ongoing() == True).paginate(
         page, per_page=current_app.config["EVENTS_PER_PAGE"], error_out=False
     )
-   
+
     events = [(event.main_image, event) for event in pagination.items]
-    
     return render_template(
         "main/index.html", events=events, form=form, pagination=pagination
     )
 
 
-@main.route("/", methods=["POST"])
+@main.route("/advanced-search", methods=["GET"])
 def advanced_search():
     """View function to handle queries from the advanced search form
     on the home page."""
     search_endpoint = "main.advanced_search"
     form = AdvancedSearchForm()
     page = request.args.get("page", 1, type=int)
-    if form.validate_on_submit():
-        state = AdvancedSearchForm.convert_choice_to_value(form.state.data, "STATES")
-        query = services.build_event_search_query(
-            state, form.start_date.data, form.end_date.data
-        )
-        form_data = {"category": form.category.data, "city": form.city.data}
-        results = services.execute_advanced_event_search(
-            query,
-            form_data,
-            search_endpoint,
-            page,
-            current_app.config["EVENTS_PER_PAGE"],
-        )
-        return render_template(
-            "main/search.html",
-            events=results["pagination"].items,
-            prev_url=results["prev_url"],
-            next_url=results["next_url"],
-            pagination=results["pagination"],
-        )
-    pagination = services.get_live_events(current_app.config["EVENTS_PER_PAGE"])
-    events = pagination.items
-    if events:
-        events = [
-            (event.main_image, event) 
-            for event in events if event.is_ongoing()
-        ]
-    return render_template(
-        "main/index.html", events=events, form=form, pagination=pagination
+    form_data = form.data
+    state_id = form.state.data
+    form_data["state"] = AdvancedSearchForm.convert_choice_to_value(form.state.data, "STATES")
+    search_request = services.create_advanced_event_search_request(form_data)
+    results = paginate_search(
+        Event,
+        search_request,
+        search_endpoint,
+        page,
+        current_app.config["EVENTS_PER_PAGE"],
     )
+    fragment = f'''
+        query={state_id}
+        &{form_data["city"]}
+        &{form_data["start_date"]}
+        &{form_data["end_date"]}
+        &{form_data["category"]}&
+    '''
+    events = [
+        (event.main_image, event)
+        for event in results["events"]
+    ]
+    print(events)
+    return render_template(
+        "main/search.html",
+        events=events,
+        endpoint=search_endpoint,
+        pagination=results["pagination"],
+        prev_url=results["prev_url"],
+        next_url=results["next_url"],
+        fragment=fragment,
+    )
+    
+    
 
 
 @main.route("/search")
 def search():
     """Return search results."""
-
-
-    endpoint = "main.search"
+    search_endpoint = "main.search"
     page = request.args.get("page", 1, type=int)
+    search_request = services.create_simple_event_search_request(
+        g.search_form.query.data, "title"
+    )
     results = paginate_search(
         Event,
-        g.search_form.query.data,
-        "main.search",
+        search_request,
+        search_endpoint,
         page,
         current_app.config["EVENTS_PER_PAGE"],
     )
-    events = results["pagination"].items
-    # Filter for live events only
-    if events:
-        events = [
-            (event.main_image, event) 
-            for event in events if event.is_ongoing()
-        ]
+    events = [
+        (event.main_image, event)
+        for event in results["events"]
+    ]
     return render_template(
         "main/search.html",
         events=events,
-        endpoint=endpoint,
+        endpoint=search_endpoint,
         pagination=results["pagination"],
         prev_url=results["prev_url"],
         next_url=results["next_url"],
+        fragment=f"query={g.search_form.query.data}&",
     )
 
