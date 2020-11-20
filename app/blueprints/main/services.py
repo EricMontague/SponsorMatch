921596491b94e07cd0929e24afc88fd5e3c6d1c3
Eric Montague <eric.g.montague@gmail.com>
2020-11-19 23:07:07 -0500

    Removed unecessary imports and refactored the advanced search function to use the newly create BooleanQuery class

--- app/blueprints/main/services.py
+++ app/blueprints/main/services.py
@@ -2,66 +2,48 @@
 the main blueprint.
 """
 
-from sqlalchemy import func
-from app.models import Event, Venue
-from flask import url_for
-from app.common import QueryType
+from app.search import BooleanQuery, QueryType, BooleanClause
 
 
-
-def create_simple_event_search_request(query, field):
-    search_request = {
-        "query": query,
-        "field": field,
-        "query_type": QueryType.MATCH,
-    }
-    return search_request
-
-
-
+    
 def create_advanced_event_search_request(form_data):
-    search_request = {
-        "query_type": QueryType.BOOL,
-        "must": [
-            {
-                "term": {
-                    "event_category_id": {
-                        "value": form_data["category"]
-                    }
-                }
-            },
-            {
-                "match": {
-                    "venue.city": {
-                        "query": form_data["city"]
-                    }
-                }
-            },
-            {
-                "match": {
-                    "venue.state": {
-                        "query": form_data["state"]
-                    }
-                }
-            },
-            {
-                "range": {
-                    "start_datetime": {
-                        "gte": form_data["start_date"],
-                        "lte": form_data["start_date"],
-                        "format": "yyyy-MM-dd"
-                    }
-                }
-            },
-            {
-                "range": {
-                    "end_datetime": {
-                        "gte": form_data["end_date"],
-                        "lte": form_data["end_date"],
-                        "format": "yyyy-MM-dd"
-                    }
-                }
-            },
-        ]
-    }
-    return search_request
+    bool_query = BooleanQuery()
+    bool_query.add(
+        BooleanClause.MUST, 
+        QueryType.TERM,
+        "event_category_id",
+        {"value": form_data["category"]}
+    )
+    bool_query.add(
+        BooleanClause.MUST,
+        QueryType.MATCH,
+        "venue.city",
+        {"query": form_data["city"]}
+    )
+    bool_query.add(
+        BooleanClause.MUST,
+        QueryType.MATCH,
+        "venue.state",
+        {"query": form_data["state"]}
+    )
+    bool_query.add(
+        BooleanClause.MUST,
+        QueryType.RANGE,
+        "start_datetime",
+        {
+            "gte": form_data["start_date"],
+            "lte": form_data["end_date"],
+            "format": "yyyy-MM-dd"   
+        }   
+    )
+    bool_query.add(
+        BooleanClause.MUST,
+        QueryType.RANGE,
+        "end_datetime",
+        {
+            "gte": form_data["start_date"],
+            "lte": form_data["end_date"],
+            "format": "yyyy-MM-dd"
+        }
+    )
+    return bool_query
