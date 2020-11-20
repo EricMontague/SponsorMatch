f018caab7c9813cb4222edd8b834abbfa3506538
Eric Montague <eric.g.montague@gmail.com>
2020-11-19 23:04:50 -0500

    Renamed directory from common to utils

---
+++ app/utils/__init__.py
@@ -0,0 +1,8 @@
+"""This package contains modules that contain helper functions and classes
+to be used in the application.
+"""
+
+
+from app.utils.decorators import permission_required, admin_required
+from app.utils.email import send_email
+
