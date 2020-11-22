861a0c4fc1571ab267c2955e950c1aa95fa5ff59
Eric Montague <eric.g.montague@gmail.com>
2020-11-22 00:33:34 -0500

    Changed field name to filter

--- app/blueprints/dashboard/forms.py
+++ app/blueprints/dashboard/forms.py
@@ -8,10 +8,10 @@ from wtforms import SelectField
 class DropdownForm(FlaskForm):
     """Form to represent a dropdown menu of options."""
 
-    options = SelectField(
+    filter = SelectField(
         choices=[(1, "All"), (2, "Live"), (3, "Past"), (4, "Draft")], coerce=int
     )
 
     def __init__(self, choices, **kwargs):
         super(DropdownForm, self).__init__(**kwargs)
-        self.options.choices = choices
+        self.filter.choices = choices
