67a750b70154155fda8e31ee9376b28e7c80a2c7
Eric Montague <eric.g.montague@gmail.com>
2020-11-22 23:22:05 -0500

    Changed the creation flow for the elasticsearch middleware to be instantiated when the app is created

--- app/search/__init__.py
+++ app/search/__init__.py
@@ -2,6 +2,7 @@
 interface over the low level elasticsearch client.
 """
 
-from app.search.middleware import sqlalchemy_search_middleware
+from app.search.middleware import FlaskSQLAlchemyMiddleware
+from app.search.client import ElasticsearchClient
 from app.search.query import MatchQuery, BooleanQuery
 from app.search.constants import QueryType, BooleanClause
